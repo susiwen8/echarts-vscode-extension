b330d586153eb5004eea255239d985db96a180cb
susiwen <susiwen@baidu.com>
2020-01-01 19:42:35 +0800

    Title options

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import titleOptions from './options/title';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "echarts" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.echarts', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('ECharts autocomplete is up!');
 	});
 
-	const completion = vscode.languages.registerCompletionItemProvider('javascript', {
+	const selector: vscode.DocumentSelector = {
+		scheme: 'file',
+		language: 'javascript'
+	};
+
+	const completion = vscode.languages.registerCompletionItemProvider(selector, {
 		provideCompletionItems(
 			document: vscode.TextDocument,
 			position: vscode.Position
 		) {
 			let linePrefix = document.lineAt(position).text.substr(0, position.character);
 			if (linePrefix.endsWith('title:')) {
-				return [
-					new vscode.CompletionItem('{}', vscode.CompletionItemKind.Method)
-				];
+				return titleOptions;
 			}
 		}
-	});
+	}, ':');
 
-	context.subscriptions.push(completion);
+	context.subscriptions.push(disposable, completion);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
