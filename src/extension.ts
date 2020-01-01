91d3908ab79649fa77c7ef7ddd9e66547e4a26c4
susiwen <susiwen@baidu.com>
2020-01-01 09:30:12 +0800

    First commit

---
+++ src/extension.ts
@@ -0,0 +1,41 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "echarts" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.echarts', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('ECharts autocomplete is up!');
+	});
+
+	const completion = vscode.languages.registerCompletionItemProvider('javascript', {
+		provideCompletionItems(
+			document: vscode.TextDocument,
+			position: vscode.Position
+		) {
+			let linePrefix = document.lineAt(position).text.substr(0, position.character);
+			if (linePrefix.endsWith('title:')) {
+				return [
+					new vscode.CompletionItem('{}', vscode.CompletionItemKind.Method)
+				];
+			}
+		}
+	});
+
+	context.subscriptions.push(completion);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
