9d59f991afb27f677e0c01c492db640dfa7b9d1c
susiwen <susiwen@baidu.com>
2020-01-02 19:21:08 +0800

    Add more url

--- src/urls.ts
+++ src/urls.ts
@@ -1,6 +1,47 @@
+/**
+ * @file option description request url
+ */
+
 const ZH_HOST: string = 'https://echarts.apache.org/zh/documents/option-parts/';
 
 export const urls = {
     TITLE_URL: `${ZH_HOST}option.title.json?`,
-    LEGEND_URL: `${ZH_HOST}option.legend.json?`
+    LEGEND_URL: `${ZH_HOST}option.legend.json?`,
+    GRID_URL: `${ZH_HOST}option.grid.json`,
+    XAXIS_URL: `${ZH_HOST}option.xAxis.json`,
+    YAXIS_URL: `${ZH_HOST}option.yAxis.json`,
+    POLAR_URL: `${ZH_HOST}option.polar.json`,
+    RADIUSAXIS_URL: `${ZH_HOST}option.radiusAxis.json`,
+    ANGLEAXIS_URL: `${ZH_HOST}option.angleAxis.json`,
+    RADAR_URL: `${ZH_HOST}option.radar.json`,
+    // _URL: `${ZH_HOST}option..json`,
+    VISUALMAP: `${ZH_HOST}option.visualMap.json`,
+    TOOLTIP_URL: `${ZH_HOST}option.tooltip.json`,
+    AXISPOINTER_URL: `${ZH_HOST}option.axisPointer.json`,
+    TOOLBOX_URL: `${ZH_HOST}option.toolbox.json`,
+    BRUSH_URL: `${ZH_HOST}option.brush.json`,
+    GEO_URL: `${ZH_HOST}option.geo.json`,
+    PARALLEL_URL: `${ZH_HOST}option.parallel.json`,
+    PARALLELAXIS_URL: `${ZH_HOST}option.parallelAxis.json`,
+    SINGLEAXIS_URL: `${ZH_HOST}option.singleAxis.json`,
+    TIMELINE_URL: `${ZH_HOST}option.timeline.json`,
+    GRAPHIC_URL: `${ZH_HOST}option.graphic.json`,
+    CALENDAR: `${ZH_HOST}option.calendar.json`,
+    DATASET_URL: `${ZH_HOST}option.dataset.json`,
+    ARIA_URL: `${ZH_HOST}option.aria.json`,
+    TEXTSTYLE_URL: `${ZH_HOST}option.textStyle.json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
+    // _URL: `${ZH_HOST}option..json`,
 };
