72841456cf65b42ed6595171165ef3e1e9b9a001
susiwen8 <susiwen8@gmail.com>
2020-02-27 20:26:56 +0800

    WIP: get ccompletion base on final option chain

--- src/extension.ts
+++ src/extension.ts
@@ -33,8 +33,14 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     const completion = vscode.languages.registerCompletionItemProvider(selector,
         {
             provideCompletionItems() {
-                // TODO
-                console.log(optionsStruct[option]);
+                console.log(option);
+                const completionItems = optionsStruct[option].map(item => {
+                    const completionItem = new vscode.CompletionItem(item, vscode.CompletionItemKind.Keyword);
+                    completionItem.insertText = new vscode.SnippetString(`${item}: $0,`);
+                    return completionItem;
+                });
+
+                return completionItems;
             }
         },
         ...generateAToZArray()
@@ -48,29 +54,6 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
             const literal = findNodeAround(ast, position, 'Literal');
             const property = findNodeAround(ast, position, 'Property');
 
-            if (
-                property
-                && isProperty(property.node)
-                && event.contentChanges[0].text.includes('\n')
-            ) {
-                option = `${walkNodeRecursive(ast, property.node, position)}.${property?.node?.key?.name}`;
-                option = option.replace(/.rich.(\S*)/, '.rich.<style_name>');
-                console.log(option);
-                return;
-            }
-
-            // Literal value is one of chart types and Property is type
-            // Then we know right now input is in series option
-            if (
-                literal && isLiteral(literal.node)
-                && CHART_TYPE.includes(literal.node.value)
-                && property && isProperty(property.node)
-                && property.node.key.name === 'type'
-            ) {
-                option = `type${literal.node.value}`;
-                return;
-            }
-
             // input is in Literal, then don't show completion
             if (literal && isLiteral(literal.node)) {
                 option = '';
@@ -83,27 +66,24 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
                 && property && isProperty(property.node)
                 && property.node.key.name === 'series'
             ) {
-                // input at 'Property', which should give CompletionItem
-                // option = property?.node?.key?.name;
-                option = `type${findChartType(property?.node.value, position)}`;
+                option = `series.${findChartType(property?.node.value, position)}`;
                 return;
             }
 
-            // Hit enter and input is not in series
+            // input at somewhere other than series
             if (
-                event.contentChanges[0].text.includes('\n')
-                && property && isProperty(property.node)
-                && property.node.key.name !== 'series'
+                property
+                && isProperty(property.node)
+                && event.contentChanges[0].text.includes('\n')
             ) {
-                // input at 'Property', which should give CompletionItem
-                option = property?.node?.key?.name;
+                const recursiveName = walkNodeRecursive(ast, property.node, position);
+                option = `${recursiveName}${recursiveName ? '.' : ''}${property?.node?.key?.name}`;
+                option = option.replace(/.rich.(\S*)/, '.rich.<style_name>');
                 return;
             }
 
         } catch (error) {
-            // In the case of parse error
-            // downgrade to use position line
-            option = 'error';
+            option = '';
             console.error('Parse error');
         }
     });
